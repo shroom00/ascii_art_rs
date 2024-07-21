931dfb60a8c2c6218860945340e3bdee9c4f99c5
shroom00 <124786038+shroom00@users.noreply.github.com>
2024-07-21 14:25:21 +0100

    Added colour argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use clap::{value_parser, Arg, Command};
 use nannou::{
-    image::{imageops, open, GrayImage},
+    image::{imageops, open, GenericImageView, GrayImage, RgbImage},
     prelude::*,
 };
 
+enum ImageType {
+    Gray(GrayImage),
+    Rgb(RgbImage),
+}
+
 struct Model {
     fp: String,
-    img: GrayImage,
+    img: ImageType,
     steps: usize,
     chars: Option<Vec<char>>,
 }
@@ -29,7 +34,8 @@ fn model(app: &App) -> Model {
         Arg::new("steps").short('s').value_name("NUMBER").help("How many letters to place.").value_parser(value_parser!(usize)).default_value("500"),
         Arg::new("width").long("width").value_name("NUMBER").help("The width of the output image.").value_parser(value_parser!(u32)).conflicts_with("height"),
         Arg::new("height").long("height").value_name("NUMBER").help("The height of the output image.").value_parser(value_parser!(u32)).default_value("1000"),
-        Arg::new("characters").long("chars").value_name("CHARACTERS").help("The characters you want to be used in output image. (Defaults to visible ASCII characters)")
+        Arg::new("characters").long("chars").value_name("CHARACTERS").help("The characters you want to be used in output image. (Defaults to visible ASCII characters)"),
+        Arg::new("colour").long("colour").help("Uses accurate colours when specified, instead of grayscale.").action(clap::ArgAction::SetTrue)
         ]
     ).get_matches();
 
@@ -48,12 +54,17 @@ fn model(app: &App) -> Model {
         None => (u32::MAX, *args.get_one::<u32>("height").unwrap()),
     };
 
-    let img: GrayImage = open(input)
-        .unwrap()
-        .resize(width, height, imageops::FilterType::CatmullRom)
-        .to_luma8();
-
-    let (width, height) = (img.width(), img.height());
+    let (width, height, img) = {
+        let img = open(input)
+            .unwrap()
+            .resize(width, height, imageops::FilterType::CatmullRom);
+        let (width, height) = img.dimensions();
+        let img = match args.get_flag("colour") {
+            true => ImageType::Rgb(img.to_rgb8()),
+            false => ImageType::Gray(img.to_luma8()),
+        };
+        (width, height, img)
+    };
 
     app.new_window()
         .size_pixels(width, height)
@@ -76,16 +87,22 @@ fn view(app: &App, model: &Model, frame: Frame) {
         let (w, h) = app.main_window().inner_size_pixels();
         let w = w as i64;
         let h = h as i64;
-        let (x, y) = (
-            random_range(-w / 2, w / 2),
-            random_range(-h / 2, h / 2),
-        );
+        let (x, y) = (random_range(-w / 2, w / 2), random_range(-h / 2, h / 2));
         let draw = app.draw();
 
-        let lightness = model
-            .img
-            .get_pixel((x + (w / 2)) as u32, (y + (h / 2)) as u32)
-            .0[0];
+        let colour = {
+            let (x, y) = ((x + (w / 2)) as u32, (y + (h / 2)) as u32);
+            match &model.img {
+                ImageType::Gray(img) => {
+                    let lightness = img.get_pixel(x, y).0[0];
+                    gray(lightness)
+                }
+                ImageType::Rgb(img) => {
+                    let p = img.get_pixel(x, y).0;
+                    srgb(p[0], p[1], p[2])
+                },
+            }
+        };
 
         let random_char = match &model.chars {
             Some(chars) => chars[random_range(0, chars.len())],
@@ -94,7 +111,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
         draw.text(&random_char.to_string())
             .x_y(x as f32, -y as f32)
-            .color(gray(lightness))
+            .color(colour)
             .font_size(random_range(10, 20))
             .center_justify();
         draw.to_frame(app, &frame).unwrap();
